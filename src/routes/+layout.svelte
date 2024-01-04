34127b25b61375b057c590711dda1ef1accab393
dhan4043 <dhan4043@gmail.com>
2024-01-04 17:16:49 -0500

    Imported Charis SIL font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
   import Header from '$lib/components/organisms/Header.svelte';
   import Footer from '$lib/components/organisms/Footer.svelte';
   import '$lib/styles/style.scss';
+  import '$lib/styles/blog.scss';
 </script>
 
 <Header/>
