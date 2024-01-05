9aed1b9ac81b6081fbf772d5943415c2ac54584f
dhan4043 <dhan4043@gmail.com>
2024-01-05 14:33:15 -0500

    Changed adapter to static in svelte.config

--- .svelte-kit/generated/client/nodes/4.js
+++ .svelte-kit/generated/client/nodes/4.js
@@ -1,3 +1,3 @@
-import * as universal from "../../../../src/routes/blog/[slug]/+page.js";
+import * as universal from "../../../../src/routes/blog/+page.js";
 export { universal };
-export { default as component } from "../../../../src/routes/blog/[slug]/+page.svelte";
+export { default as component } from "../../../../src/routes/blog/+page.svelte";
