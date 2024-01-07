15b13b8bebcf4c229b55416490dff7626739b159
dhan4043 <dhan4043@gmail.com>
2024-01-07 16:54:57 -0500

    Added additional styles and rehype plugins for blogposts

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,7 +19,8 @@ const config = {
   preprocess: [
     sveltePreprocess(),
     mdsvex({
-      extensions: ['.md']
+      extensions: ['.md'],
+      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
     })
   ]
 };
